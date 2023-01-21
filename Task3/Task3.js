2c4d9cc098611b48b9c46d0cb4eeaf195960e327
Vitaliy <vital10@inbox.ru>
2023-01-21 11:16:29 +0300

    Task3

--- Task3/Task3.js
+++ Task3/Task3.js
@@ -1,11 +1,63 @@
-// Задание 3
-// Реализовать чат на основе эхо-сервера wss://echo-ws-service.herokuapp.com.
-// Интерфейс состоит из input, куда вводится текст сообщения, и кнопки «Отправить».
-// При клике на кнопку «Отправить» сообщение должно появляться в окне переписки.
-// Эхо-сервер будет отвечать вам тем же сообщением, его также необходимо выводить в чат:
+function loadChat() {
+  const webSocketUri = "wss://echo-ws-service.herokuapp.com"
+  const socketInfo = document.querySelector(".socket_info")
+  const chatOutput = document.querySelector(".chat_output")
+  const sendBtn = document.querySelector(".send_btn")
+  const input = document.querySelector(".input")
+  const geoBtn = document.querySelector(".geo_btn")
 
-// Добавить в чат механизм отправки геолокации:
+  sendBtn.addEventListener("click", sendMessage)
+  geoBtn.addEventListener("click", sendGeolocation)
 
-// При клике на кнопку «Геолокация» необходимо отправить данные серверу и 
-// вывести в чат ссылку на https://www.openstreetmap.org/ с вашей геолокацией. 
-// Сообщение, которое отправит обратно эхо-сервер, выводить не нужно.
+  let webSocket = new WebSocket(webSocketUri)
+
+  webSocket.onopen = () => {
+    socketInfo.innerText = "Соединение установлено"
+  }
+
+  webSocket.onerror = () => {
+    socketInfo.textContent = "Во время соединения произошла ошибка"
+  }
+
+  webSocket.onmessage = (event) => {
+    console.log(event)
+    writeToChat(event.data, true)
+  }
+
+  function writeToChat(message, isRecived) {
+    let messageHTML = `<div class="chat_message ${
+      isRecived ? "recived" : "send"
+    }">${message}</div>`
+    chatOutput.innerHTML += messageHTML
+  }
+
+  function sendMessage() {
+    webSocket.send(input.value)
+    writeToChat(input.value, false)
+    input.value = ""
+  }
+
+  function sendGeolocation() {
+    setGeolocation()
+  }
+  function setGeolocation() {
+    if (!navigator.geolocation) {
+      writeToChat("Geolocation не поддерживается вашим браузером")
+    } else {
+      writeToChat("Определение местоположения…")
+      navigator.geolocation.getCurrentPosition(success, error)
+    }
+  }
+  
+  const success = (position) => {
+    const latitude = position.coords.latitude
+    const longitude = position.coords.longitude
+    const linkToCords = `<a href="https://www.openstreetmap.org/#map=18/${latitude}/${longitude}" target="_blank">Ссылка на координаты</a>`
+    writeToChat(linkToCords)
+  }
+  const error = () => {
+    writeToChat("Невозможно получить ваше местоположение")
+  }
+}
+
+document.addEventListener("DOMContentLoaded", loadChat)
